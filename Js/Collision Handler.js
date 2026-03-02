cc80db667ccc67bfd0b8fbe1f81147b730c17209
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-02 10:21:32 +0530

    Update Collision Handler.js

--- Js/Collision Handler.js
+++ Js/Collision Handler.js
@@ -2,9 +2,11 @@ class CollisionHandler {
     static check(player, ground) {
         player.onGround = false;
         ground.platforms.forEach(p => {
-            // Logic to detect if ninja is touching a platform
-            if (player.x < p.x + p.w && player.x + player.w > p.x &&
-                player.y + player.h > p.y && player.y + player.h < p.y + 25) {
+            if (player.x < p.x + p.w &&
+                player.x + player.w > p.x &&
+                player.y + player.h > p.y &&
+                player.y + player.h < p.y + 30) {
+                
                 player.y = p.y - player.h;
                 player.dy = 0;
                 player.onGround = true;
@@ -12,4 +14,3 @@ class CollisionHandler {
         });
     }
 }
-
