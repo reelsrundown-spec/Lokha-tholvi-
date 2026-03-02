7c28be4ce55a5b85d1c35ddfa3e1f9535925444c
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-02 14:37:06 +0530

    Update TileGround.js

--- Js/TileGround.js
+++ Js/TileGround.js
@@ -2,16 +2,16 @@ class TileGround {
     constructor(ctx) {
         this.ctx = ctx;
         this.platforms = [
-            { x: 0, y: 300, w: 600, h: 200 },
-            { x: 700, y: 220, w: 300, h: 30 },
-            { x: 1100, y: 150, w: 300, h: 30 },
-            { x: 1500, y: 300, w: 1200, h: 200 }
+            { x: 0, y: 350, w: 1000, h: 200 },
+            { x: 1100, y: 250, w: 400, h: 40 },
+            { x: 1600, y: 150, w: 400, h: 40 },
+            { x: 2100, y: 350, w: 2000, h: 200 }
         ];
         this.coins = [];
-        for(let i=0; i<6; i++) {
-            this.coins.push({ x: 300 + (i * 400), y: 200, collected: false });
+        for(let i=0; i<8; i++) {
+            this.coins.push({ x: 400 + (i * 450), y: 250, collected: false });
         }
-        this.portal = { x: 2500, y: 200, w: 60, h: 100, active: false };
+        this.portal = { x: 3800, y: 230, w: 80, h: 120, active: false };
     }
 
     draw() {
@@ -26,7 +26,7 @@ class TileGround {
         this.coins.forEach(c => {
             if (!c.collected) {
                 this.ctx.beginPath();
-                this.ctx.arc(c.x, c.y, 10, 0, Math.PI * 2);
+                this.ctx.arc(c.x, c.y, 12, 0, Math.PI * 2);
                 this.ctx.fill();
             }
         });
@@ -34,6 +34,9 @@ class TileGround {
         if (this.portal.active) {
             this.ctx.fillStyle = "purple";
             this.ctx.fillRect(this.portal.x, this.portal.y, this.portal.w, this.portal.h);
+            this.ctx.strokeStyle = "cyan";
+            this.ctx.lineWidth = 5;
+            this.ctx.strokeRect(this.portal.x, this.portal.y, this.portal.w, this.portal.h);
         }
     }
 }
