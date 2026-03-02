5362e69ec3e660edf98e5bb602caa71caecf8fd3
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-02 11:20:35 +0530

    Update CollisionHandler.js

--- Js/CollisionHandler.js
+++ Js/CollisionHandler.js
@@ -3,14 +3,14 @@ class CollisionHandler {
         player.onGround = false;
         
         ground.platforms.forEach(p => {
-            // Check if player is falling and within the horizontal bounds of the platform
+            // Check if ninja is falling and is within the horizontal range of the platform
             if (player.dy >= 0 &&
-                player.x + player.w * 0.3 < p.x + p.w &&
-                player.x + player.w * 0.7 > p.x &&
+                player.x + (player.w * 0.2) < p.x + p.w &&
+                player.x + (player.w * 0.8) > p.x &&
                 player.y + player.h > p.y &&
-                player.y + player.h < p.y + p.h + 20) {
+                player.y + player.h < p.y + 40) {
                 
-                // Set player position exactly on top of the platform
+                // Stop the ninja on top of the platform
                 player.y = p.y - player.h;
                 player.dy = 0;
                 player.onGround = true;
