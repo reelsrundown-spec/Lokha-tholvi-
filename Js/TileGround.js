d4ad4bd463f34f383208d642654776a8f4a76c45
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-02 11:26:31 +0530

    Update TileGround.js

--- Js/TileGround.js
+++ Js/TileGround.js
@@ -1,16 +1,23 @@
 class TileGround {
     constructor(ctx) {
         this.ctx = ctx;
+        // Creating a full level layout with multiple platforms
         this.platforms = [
-            { x: 0, y: 250, w: 500, h: 100 },
-            { x: 600, y: 180, w: 200, h: 30 },
-            { x: 900, y: 250, w: 600, h: 200 }
+            { x: 0, y: 300, w: 500, h: 150 },    // Starting Floor
+            { x: 600, y: 220, w: 200, h: 30 },   // First Jump
+            { x: 900, y: 150, w: 200, h: 30 },   // Higher Jump
+            { x: 1200, y: 300, w: 600, h: 150 }, // Second Main Floor
+            { x: 1900, y: 200, w: 300, h: 40 }   // Final Platform
         ];
     }
+
     draw() {
         this.platforms.forEach(p => {
+            // Draw Brown soil
             this.ctx.fillStyle = "#5d4037";
             this.ctx.fillRect(p.x, p.y, p.w, p.h);
+            
+            // Draw Green grass on top
             this.ctx.fillStyle = "#4caf50";
             this.ctx.fillRect(p.x, p.y, p.w, 15);
         });
