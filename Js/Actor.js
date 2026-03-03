0f74e3cd74dbc41ef72ac69468011b591e622854
reelsrundown-spec <reelsrundown@gmail.com>
2026-03-03 07:58:04 +0530

    Update Actor.js

--- Js/Actor.js
+++ Js/Actor.js
@@ -4,39 +4,24 @@ class Actor {
         this.img = new Image();
         this.img.src = "Images/player_run.gif"; 
         this.x = 100;
-        this.y = 100;
+        this.y = 350; // ഹീറോയെ തറയിൽ നിർത്താൻ ഇത് സഹായിക്കും
         this.w = 90;
         this.h = 110;
         this.dx = 0;
         this.dy = 0;
         this.dir = 1;
         this.onGround = false;
-        this.imageLoaded = false;
-
-        this.img.onload = () => {
-            this.imageLoaded = true;
-        };
-        
-        this.img.onerror = () => {
-            this.imageLoaded = false;
-        };
     }
 
     draw() {
-        if (this.imageLoaded) {
-            this.ctx.save();
-            if (this.dir === -1) {
-                this.ctx.scale(-1, 1);
-                this.ctx.drawImage(this.img, -this.x - this.w, this.y, this.w, this.h);
-            } else {
-                this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-            }
-            this.ctx.restore();
+        this.ctx.save();
+        if (this.dir === -1) {
+            this.ctx.scale(-1, 1);
+            this.ctx.drawImage(this.img, -this.x - this.w, this.y, this.w, this.h);
         } else {
-            // Fallback: Blue box if image fails
-            this.ctx.fillStyle = "blue";
-            this.ctx.fillRect(this.x, this.y, this.w, this.h);
+            this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
         }
+        this.ctx.restore();
     }
 
     update() {
